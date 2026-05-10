715f2d82e60ba13bfc280976af1e41053fcab151
johnmorrice2022-code <johnmorrice2022@gmail.com>
2026-05-10 15:13:32 +0100

    Fix SVG height for probability tree coordinate mapping

--- src/components/diagrams/ProbabilityTree.tsx
+++ src/components/diagrams/ProbabilityTree.tsx
@@ -368,6 +368,7 @@ export function ProbabilityTree({
   return (
     <svg
       width="100%"
+      height="100%"
       viewBox={`0 ${VB_Y} ${VB_WIDTH} ${VB_HEIGHT}`}
       data-vb={`0 ${VB_Y} ${VB_WIDTH} ${VB_HEIGHT}`}
       role="img"
